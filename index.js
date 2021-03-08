685e76361dd92ca16e1a32527bcde3ed3a90f63b
jakazzy <jakazzy@gmail.com>
2021-03-08 13:58:52 +0000

    update the index file

--- index.js
+++ index.js
@@ -8,17 +8,14 @@ module.exports = (app) => {
 
 
     app.on(["issues.opened", "issues.edited"], async(context) => {
-      const {title, body, labels, repository_url }= await context.payload.issue
+      const {title, body, labels, repository_url }=  context.payload.issue
       const IssueLabels = labels.map(label => label.name)
       if(IssueLabels.includes("help wanted")){
-        const params = context.issue({ body, title, repository_url });
-        return await context.octokit.issues.createComment(params);
+        context.log.info(title, body, labels, repository_url)
       }
-
-      
     
     });
-    app.log.info("So is this how it will be")
+    
   
   
 
