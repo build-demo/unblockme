fdd3ac95f29c7c5434cf826f4a8bc7daec531036
jakazzy <jakazzy@gmail.com>
2021-03-08 13:49:26 +0000

    respond with a comment on issue created

--- index.js
+++ index.js
@@ -7,53 +7,18 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   module.exports = app => {
-    app.on('pull_request.opened', async context => {
-      const files = await context.github.pullRequests.getFiles(context.issue())
-      const config = await context.config('config.yml')
-      context.log.info(context.payload);
-      context.log.info(config);
-      context.log.info(files);
-      // const docs = files.data.find(function (file) {
-      //   let targetFile
-  
-      //   if (config.updateDocsTargetFiles) {
-      //     targetFile = config.updateDocsTargetFiles.find(function (item) {
-      //       if (file.filename.startsWith(item) || file.filename.includes(item)) {
-      //         return item
-      //       }
-      //     })
-      //     return targetFile
-      //   } else {
-      //     if (file.filename.startsWith('README') || file.filename.includes('docs/')) {
-      //       return file
-      //     }
-      //   }
-      // })
-  
-      // if (!docs) {
-      //   // Get config.yml and comment that on the PR
-      //   try {
-      //     const title = context.payload.pull_request.title
-      //     let whiteList
-      //     if (config.updateDocsWhiteList) {
-      //       whiteList = config.updateDocsWhiteList.find(function (item) {
-      //         if (title.toLowerCase().includes(item.toLowerCase())) {
-      //           return item
-      //         }
-      //       })
-      //     }
-      //     // Check to make sure it's not whitelisted (ie bug or chore)
-      //     if (!whiteList) {
-      //       const template = config.updateDocsComment
-      //       return context.github.issues.createComment(context.issue({body: template}))
-      //     }
-      //   } catch (err) {
-      //     if (err.code !== 404) {
-      //       throw err
-      //     }
-      //   }
-      // }
-    })
+    app.on(["issues.opened", "issues.edited"], (context) => {
+      const {title, body, labels, repository_url }= context.payload.issue
+      const IssueLabels = labels.map(label => label.name)
+      if(IssueLabels.includes("help wanted")){
+        const params = context.issue({ body, title, repository_url });
+        return context.octokit.issues.createComment(params);
+      }
+
+      
+    
+    });
+    app.log.info("So is this how it will be")
   }
   
 
