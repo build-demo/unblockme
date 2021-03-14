4f4167ec5a23102910cefd122465816bea177f1b
jakazzy <jakazzy@gmail.com>
2021-03-14 15:32:12 +0000

    update auth routes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const { addUser } = require('../controllers/userController')
 
 router.get('/failed', (req,res) => res.send('Failed Log In'))
 router.get('/success', (req,res) => res.send('Successful Log In '))
@@ -11,24 +12,33 @@ router.get('/google',(req, res, next)=> {
   scope:[
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/calendar',
       'https://www.googleapis.com/auth/calendar.app.created',
       'https://www.googleapis.com/auth/calendar.calendarlist.readonly',
       'https://www.googleapis.com/auth/calendar.events.freebusy',
       'https://www.googleapis.com/auth/calendar.freebusy',
-    ]
+    ],
+    accessType: 'offline', approvalPrompt: 'force'
 }));
-
 router.get('/google/callback', 
-  passport.authenticate('google', {failureRedirect: process.env.failureURL+'?auth=false'}),
-  function(req, res) {
-    if(req.user.isRegistered){
-      //These should be edited as {frontend_url/dashbaord}
-      res.redirect('/dashboard')
-    }
-    else{
-      //These should be edited as {frontend_url/register}
-      res.redirect('/register')
-    }
-});
+  passport.authenticate('google', {session: false,
+    failureRedirect: process.env.CLIENT_URL,}),
+  function(req, res){
+    // console.log(req);
+    res.redirect(`${process.env.CLIENT_URL}/profile-setup`)
+  }
+//   function(req, res) {
+//     if(req.user.isRegistered){
+//       //These should be edited as {frontend_url/dashbaord}
+//       res.redirect('/dashboard')
+//     }
+//     else{
+//       //These should be edited as {frontend_url/register}
+//       res.redirect('/register')
+//     }
+// }
+
+
+);
 
 module.exports = router;
