2bf03e0a07aa0d7ff79e407f66b04626d798b9ff
jakazzy <jakazzy@gmail.com>
2021-03-15 02:40:47 +0000

    update relative paths

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const passport = require('passport');
 const { addUser } = require('../controllers/userController')
+const { githubAuth } = require('../controllers/authController')
 
 router.get('/failed', (req,res) => res.send('Failed Log In'))
 router.get('/success', (req,res) => res.send('Successful Log In '))
@@ -22,7 +23,7 @@ router.get('/google',(req, res, next)=> {
 }));
 router.get('/google/callback', 
   passport.authenticate('google', {session: false,
-    failureRedirect: process.env.CLIENT_URL,}),
+    failureRedirect: process.env.CLIENT_URL}),
   function(req, res){
     // console.log(req);
     res.redirect(`${process.env.CLIENT_URL}/profile-setup`)
@@ -37,8 +38,27 @@ router.get('/google/callback',
 //       res.redirect('/register')
 //     }
 // }
-
-
 );
+
+
+router.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_AUTH_CLIENT_ID}&scope=user`);
+});
+
+
+
+// router.get('/github',
+//   passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+// router.get('/github/callback', 
+//   passport.authenticate('github', { session: false, failureRedirect: process.env.CLIENT_URL }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect(`${process.env.CLIENT_URL}/profile-setup`)
+//   });
+
+
+
+router.get('/github/callback', githubAuth);
 
 module.exports = router;
